5cbbf0c5745870f23dd74d71c67af18ec736c0ff
Barrett Ruth <br.barrettruth@gmail.com>
2024-06-05 09:50:30 -0500

    feat(ci): deploy w/ script

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+aws s3 sync . s3://barrettruth.com --delete \
+    --exclude ".git/*" \
+    --exclude ".github/*" \
+    --exclude "readme.md" \
+    --exclude ".DS_Store" \
+    --exclude ".gitignore" \
+    --exclude "scripts/*"
