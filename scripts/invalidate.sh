3042a7f5a7ef7c5651c50b540bd9101d090452c9
Barrett Ruth <br.barrettruth@gmail.com>
2024-06-05 09:50:46 -0500

    feat(scripts): cloudfront invalidation utility

---
+++ scripts/invalidate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+[ "$#" -lt 1 ] && echo "Usage: $0 <distribution-id> <path1> [path2 ...]" && exit 1
+
+DISTRIBUTION_ID="$1"
+
+shift
+
+aws cloudfront create-invalidate --distribution-id "$DISTRIBUTION_ID" --paths "$*"
