6297fbfb917891232fc8fc5df31be8ddf2311efd
Barrett Ruth <br.barrettruth@gmail.com>
2024-06-08 13:56:32 -0500

    fix: reset terminal prompt on  back arrow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,4 @@ aws s3 sync . s3://barrettruth.com --delete \
     --exclude ".github/*" \
     --exclude "readme.md" \
     --exclude ".DS_Store" \
-    --exclude ".gitignore" \
-    --exclude "scripts/*"
+    --exclude ".gitignore"
