8655cd7097c7737aa7a3be0b61194bb53f5c0216
Barrett Ruth <br.barrettruth@gmail.com>
2024-06-22 10:31:34 -0500

    fix(post): only unfold first post

--- scripts/post.js
+++ scripts/post.js
@@ -6,14 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
     h2.prepend(mdHeading);
   });
 
-  document.querySelectorAll(".problem-header h3").forEach((h3) => {
+  document.querySelectorAll(".problem-header h3").forEach((h3, i) => {
     const toggle = document.createElement("span");
-    toggle.textContent = "v";
+    toggle.textContent = i === 0 ? "v" : ">";
+    h3.parentElement.nextElementSibling.style.display =
+      toggle.textContent === ">" ? "none" : "block";
     toggle.classList.add("problem-toggle");
     toggle.addEventListener("click", () => {
       const content = h3.parentElement.nextElementSibling;
-      content.style.display = toggle.textContent === ">" ? "block" : "none";
       toggle.textContent = toggle.textContent === ">" ? "v" : ">";
+      content.style.display = toggle.textContent === ">" ? "none" : "block";
     });
 
     const mdHeading = document.createElement("span");
