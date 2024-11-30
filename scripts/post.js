ed0c3b7ab2fd148fb767dc5630ffaabb9bafe3fe
Barrett Ruth <br.barrettruth@gmail.com>
2024-11-29 19:38:44 -0600

    feat(posts): remove folds

--- scripts/post.js
+++ scripts/post.js
@@ -8,28 +8,6 @@ const tagToHeader = new Map([
   ["H3", "##"],
 ]);
 
-const makeFold = (h, i) => {
-  const toggle = document.createElement("span");
-  toggle.style.fontStyle = "normal";
-  toggle.textContent = ">";
-
-  // only unfold first algorithm problem
-  if (urlToTopic() === "algorithms" && i === 0) toggle.textContent = "v";
-
-  h.parentElement.nextElementSibling.style.display =
-    toggle.textContent === ">" ? "none" : "block";
-  h.parentE;
-  toggle.classList.add("fold-toggle");
-  toggle.addEventListener("click", () => {
-    const content = h.parentElement.nextElementSibling;
-    toggle.textContent = toggle.textContent === ">" ? "v" : ">";
-    content.style.display = toggle.textContent === ">" ? "none" : "block";
-  });
-  toggle.style.color = getTopicColor(urlToTopic());
-
-  h.prepend(toggle);
-};
-
 const setStyle = (h) => {
   const mdHeading = document.createElement("span");
   const header = tagToHeader.has(h.tagName) ? tagToHeader.get(h.tagName) : "";
@@ -41,6 +19,4 @@ const setStyle = (h) => {
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".post-article h2").forEach(setStyle);
   document.querySelectorAll(".post-article h3").forEach(setStyle);
-  document.querySelectorAll(".fold h2").forEach(makeFold);
-  document.querySelectorAll(".fold h3").forEach(makeFold);
 });
