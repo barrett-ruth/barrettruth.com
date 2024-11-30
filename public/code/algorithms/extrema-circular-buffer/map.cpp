ed8325560470f59b7c1cea4c86d355a9d68245b4
Barrett Ruth <br.barrettruth@gmail.com>
2024-11-29 21:27:21 -0600

    fix(post): fetch code lazily

---
+++ public/code/algorithms/extrema-circular-buffer/map.cpp
@@ -0,0 +1,56 @@
+#include <deque>
+#include <map>
+#include <stdexcept>
+
+class ExtremaCircularBuffer {
+public:
+  ExtremaCircularBuffer(size_t capacity) : capacity(capacity) {}
+
+  void push_back(double value) {
+    if (prices.size() == capacity) {
+      double front = prices.front();
+
+      if (--sorted_prices[front] == 0)
+        sorted_prices.erase(front);
+      prices.pop_front();
+    }
+
+    prices.push_back(value);
+    ++sorted_prices[value];
+  }
+
+  void pop_front() {
+    if (prices.empty()) {
+      throw std::out_of_range("Cannot pop_front() from empty buffer");
+    }
+
+    double front = prices.front();
+
+    if (--sorted_prices[front] == 0)
+      sorted_prices.erase(front);
+    prices.pop_front();
+  }
+
+  size_t size() const { return prices.size(); }
+
+  double get_max() const {
+    if (prices.empty()) {
+      throw std::out_of_range("Cannot find max() of empty buffer");
+    }
+
+    return sorted_prices.rbegin()->first;
+  }
+
+  double get_min() const {
+    if (prices.empty()) {
+      throw std::out_of_range("Cannot find min() of empty buffer");
+    }
+
+    return sorted_prices.begin()->first;
+  }
+
+private:
+  std::deque<double> prices;
+  std::map<double, size_t> sorted_prices;
+  size_t capacity;
+};
--- scripts/post.js
+++ scripts/post.js
@@ -20,3 +20,39 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".post-article h2").forEach(setStyle);
   document.querySelectorAll(".post-article h3").forEach(setStyle);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".code").forEach(loadCode);
+});
+
+async function loadCode(e) {
+  const file = e.dataset.file;
+  const language = file.substring(file.lastIndexOf(".") + 1);
+  let [_, __, topic, post] = window.location.pathname.split("/");
+  post = post.substring(0, post.lastIndexOf("."));
+
+  try {
+    const path = `/public/code/${topic}/${post}/${file}`;
+    const response = await fetch(path);
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `Failed to fetch ${path}: ${response.status} ${response.statusText}\n${errorText}`,
+      );
+    }
+
+    const code = await response.text();
+
+    const pre = document.createElement("pre");
+    const codeElement = document.createElement("code");
+    codeElement.className = `language-${language}`;
+    codeElement.textContent = code;
+
+    pre.appendChild(codeElement);
+    e.appendChild(pre);
+
+    Prism.highlightElement(codeElement);
+  } catch (error) {
+    console.error(error);
+  }
+}
