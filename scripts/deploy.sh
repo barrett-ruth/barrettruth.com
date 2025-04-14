766346bc1d78be3b48baaf4bec53680504e9cfaf
Barrett Ruth <br.barrettruth@gmail.com>
2025-04-14 14:47:20 -0400

    feat: fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,5 @@ aws s3 sync . s3://barrettruth.com --delete \
     --exclude "readme.md" \
     --exclude ".DS_Store" \
     --exclude ".gitignore" \
-    --exclude "scripts/*.sh"
+    --exclude "scripts/*.sh" \
+    --exclude "files/*"
