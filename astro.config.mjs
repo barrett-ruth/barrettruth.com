f0c1c150028c36549dc667d66ce571bc0743b115
Barrett Ruth <br.barrettruth@gmail.com>
2025-06-22 14:22:12 -0500

    feat(proofs): some proofs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import path from "path";
 
 export default defineConfig({
   build: {
@@ -13,6 +14,13 @@ export default defineConfig({
       rehypePlugins: [rehypeKatex],
     }),
   ],
+  vite: {
+    resolve: {
+      alias: {
+        "@components": path.resolve(".", "src/components"),
+      },
+    },
+  },
   markdown: {
     shikiConfig: {
       theme: "github-light",
